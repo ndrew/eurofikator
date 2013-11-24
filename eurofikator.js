5d91c7959e483e0cec1d074390fa1b6c0a3f4457
Andrew Sernyak <ndrew.sernyak@gmail.com>
2013-11-24 02:41:09 +0200

    first version of eurofikator

---
+++ eurofikator.js
@@ -0,0 +1,31 @@
+function recursiveReplace(node) {
+    if (node.nodeType == 3) { 
+        var words = node.nodeValue.replace(/\s+/g, " ").split(/\s/);
+        var res = "";
+        for (var i = 0; i < words.length; i++) {
+            var s = words[i];
+            if (s.length > 2) {
+                var c = s.charAt(0);
+                if (c.match(/[\u0400-\u04FF]/)) {
+                    if (c == c.toLocaleUpperCase()) {
+                        res = res + "Євро" + s.toLocaleLowerCase() + " ";
+                    } else {
+                        res = res + "євро" + s + " ";                 
+                    }
+                }              
+            } else {
+                res = res + s + " ";
+            }
+        }
+        node.nodeValue = res;
+        return;
+    }
+
+    if (node.nodeType == 1) {
+        $(node).contents().each(function () {
+            recursiveReplace(this);
+        });
+    }
+}
+
+recursiveReplace(document.body);
